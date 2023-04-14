dd9e079b9a3de0c59d4ca707b7e3d11831c3ed12
Kiryuha161 <108215760+Kiryuha161@users.noreply.github.com>
2023-04-14 22:35:23 +0300

    Пользовательский интерфейс через код

--- Practic/Assets/Scripts/GameBehaviour.cs
+++ Practic/Assets/Scripts/GameBehaviour.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using Unity.UI;
+using UnityEngine.SceneManagement;
 
 public class GameBehaviour : MonoBehaviour
 {
+    [SerializeField] private bool _showWinScreen = false;
+    private string _labelText = "Collect all 4 items and you will win the game";
+    [SerializeField] private int _maxItems = 4;
     private int _itemCollected = 0;
-    public int Items 
+
+   
+
+    public int Items
     {
         get
         {
@@ -14,12 +24,24 @@ public class GameBehaviour : MonoBehaviour
 
         set
         {
+
             _itemCollected = value;
-            Debug.LogFormat($"Items: {_itemCollected}");
+            if (_itemCollected >= _maxItems)
+            {
+                _labelText = $"You win!";
+                _showWinScreen = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                _labelText = $"You need yet {_maxItems - _itemCollected} items";
+            }
+
+
         }
 
-     }
-    
+    }
+
     private int _playerHP = 10;
     public int HP
     {
@@ -33,16 +55,24 @@ public class GameBehaviour : MonoBehaviour
             Debug.LogFormat($"Lives: {_playerHP}");
         }
     }
-    
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnGUI()
     {
-        
+        GUI.Box(new Rect(20, 20, 150, 50), $"Player HP: {_playerHP}");
+        GUI.Box(new Rect(20, 70, 150, 50), $"Items collected: {_itemCollected}");
+
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), _labelText);
+
+        if (_showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height - 200, 200, 100), "You won!"))
+            {
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1f;
+            }
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
